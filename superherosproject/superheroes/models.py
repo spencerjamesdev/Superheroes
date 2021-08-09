79ebd665a7190b79c8ee6e6c9405cfbc745ee073
spencerjamesdev <spencer.franke@gmail.com>
2021-08-09 17:42:18 -0500

    Added __str__ function

--- superherosproject/superheroes/models.py
+++ superherosproject/superheroes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    primary_ability = models.CharField(max_length=50)
+    secondary_ability = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=50)
+    
