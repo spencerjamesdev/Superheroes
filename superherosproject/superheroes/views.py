d21550545b03d0f73ac050c5114cd52fff6b0051
spencerjamesdev <spencer.franke@gmail.com>
2021-08-09 21:55:51 -0500

    Added index function to views

--- superherosproject/superheroes/views.py
+++ superherosproject/superheroes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    all_superheroes = Superhero.objects.all()
+    context = {
+        'all_superheroes': all_superheroes
+    }
+    return render(request, 'superheroes/index.html', context)
 
