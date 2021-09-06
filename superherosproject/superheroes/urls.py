78f82a1d4b434e9b2a132f851bb0087046f8bd4f
spencerjamesdev <spencer.franke@gmail.com>
2021-09-05 20:17:34 -0500

    Added img

--- superherosproject/superheroes/urls.py
+++ superherosproject/superheroes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:superhero_id>/', views.detail, name='detail')
+    path('detail/<int:superhero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create'),
+    path('<int:superhero_id>/delete/', views.delete, name='delete')
 ]
--- superherosproject/superheroes/views.py
+++ superherosproject/superheroes/views.py
@@ -1,6 +1,9 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Superhero
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -12,3 +15,38 @@ def index(request):
     return render(request, 'superheroes/index.html', context)
 
 def detail(request, superhero_id):
+    superhero = Superhero.objects.get(id=superhero_id)
+    context = {
+        'superhero': superhero
+    }
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def delete(request, superhero_id):
+
+    context ={}
+ 
+    superhero = Superhero.objects.get(id = superhero_id)
+ 
+ 
+    if request.method =="POST":
+        # delete object
+        superhero.delete()
+        # after deleting redirect to
+        # home page
+        return HttpResponseRedirect(reverse('superheroes:index'))
+ 
+    return render(request, "superheroes/delete.html", context)
+
